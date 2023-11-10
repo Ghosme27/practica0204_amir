bcd2e523f51aac71642752c75b90e20bf5050ff8
Ghosme27 <113557448+Ghosme27@users.noreply.github.com>
2023-11-10 23:48:42 +0100

    .

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,5 +1,14 @@
-
-b=()
-while b!='terminar':
-    a=input('Introduzca palabras en español con su traduccion en ingles separado por :')
+traduccion={}
+while True:
+    a=input('Introduzca palabras en español con su traduccion en ingles separado por dos puntos (:) :')
     b=input('Si no quieres continuar ponga terminar,si no es asi solo dale a enter:')
+    if b=='terminar':
+        break   
+    else:
+        español,ingles=a.split(':')
+        traduccion[español]=ingles
+print ('Estas son las palabras con su traduccion que tenemos',traduccion)
+frase_traduce=input('Escriba una frase:')
+palabra_traducir=[traduccion.get(español,español) for español in  frase_traduce.split()]
+frase_enes=' '.join(palabra_traducir)
+print(frase_enes)
